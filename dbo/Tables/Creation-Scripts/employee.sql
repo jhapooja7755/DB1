161ac81a807f927e80e771101d53896143383cfd
jhapooja7755 <email>
2022-05-04 09:44:15 +0530

    Commit for employee

---
+++ dbo/Tables/Creation-Scripts/employee.sql
@@ -0,0 +1,10 @@
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[employee](
+	[FirstName] [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	[LastName] [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
+)
+
+GO

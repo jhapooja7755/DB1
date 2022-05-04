a5f6bd246efd1be5e6c55ddc12c74bdfde46ee12
jhapooja7755 <email>
2022-05-04 09:40:58 +0530

    commit new

---
+++ dbo/Tables/Creation-Scripts/Table_2.sql
@@ -0,0 +1,11 @@
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Table_2](
+	[id] [int] NULL,
+	[name] [varchar](50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	[number] [int] NULL
+)
+
+GO
